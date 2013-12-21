cea1b78a7435a03c5ebcae6706c9dcd8daac94f3
Leonardo Azopardo <lazopard@gmail.com>
2013-12-21 16:06:15 -0430

    Added structure to the page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -29,6 +29,17 @@ describe "Static pages" do
     end
   end
 
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("#{base_title} | Help")
+    end
+  end
+
   describe "Contact" do
     it "should have the content 'Contact'" do
       visit '/static_pages/contact'
